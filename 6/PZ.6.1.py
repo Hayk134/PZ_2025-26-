ffb3b9b1e6dac30771b105573fb9fa3ca76b149d
Hayk134 <ajkapundzan@gmail.com>
2025-11-15 14:15:38 +0300

    Update PZ.6.1.py

--- 6/PZ.6.1.py
+++ 6/PZ.6.1.py
@@ -2,14 +2,14 @@
 1. Дан список А размера № и целое число К (1 < К< ). Преобразовать список, увеличив каждый его элемент на исходное значение элемента K.
 '''
 
-
+import random
 def task(A, K):
    
     value = A[K - 1]
     return [x + value for x in A]
 
 
-A = [random.randit(1, 20) for _ in range(10)]
+A = [random.randint(1, 20) for _ in range(10)]
 K = 4
 result = task(A, K)
 

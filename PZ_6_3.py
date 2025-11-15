558709f69b0bcc50fe35d008038c3c2606111745
Hayk134 <ajkapundzan@gmail.com>
2025-11-15 11:56:14 +0300

    Update PZ_6_3.py

--- PZ_6_3.py
+++ PZ_6_3.py
@@ -11,6 +11,6 @@ def task(A):
 
     return [0] + A[:-1]
 
-A_2 = list(map(int,input ("Введите элементы списка А через пробел: ").split()))
-print(f"{task(A_2)}")
+A_2 = [int(x) for x in input ("Введите элементы списка А через пробел: ").split()]
 
+print(f"{task(A_2)}")

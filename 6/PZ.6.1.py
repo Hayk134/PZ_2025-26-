c05368b4c945f4454c81c8902bdf3b3e27d6a665
Hayk134 <ajkapundzan@gmail.com>
2025-11-15 14:13:18 +0300

    Update PZ.6.1.py

--- 6/PZ.6.1.py
+++ 6/PZ.6.1.py
@@ -9,7 +9,7 @@ def task(A, K):
     return [x + value for x in A]
 
 
-A = [int(x) for x in input ("Введите элементы списка А через пробел: ").split()]
+A = [random.randit(1, 20) for _ in range(10)]
 K = 4
 result = task(A, K)
 

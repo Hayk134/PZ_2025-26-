04cb437e70b0f66f5742c732abf28904fc431895
Hayk134 <ajkapundzan@gmail.com>
2025-11-29 10:19:58 +0300

    Update PZ.7.2.py

--- 7/PZ.7.2.py
+++ 7/PZ.7.2.py
@@ -2,12 +2,9 @@
 Дан символ С и строки S, S₀. После каждого вхождения символа С в строку S вставить строку S₀.
 '''
 
-def stroka (text):
-    f, l = text.find(''), text.rfind('')
-    
-    if f != l :
-        return text[f + 1 : l]
-    else :
-        return""
-test = "Тут есть много пробл в " 
-print(stroka("Тут есть много про"))
+C = 'b'
+S = 'rab'
+S0 ='XX'
+result = (C + S0).join(S.split(C))
+print(f"{result}")
+
